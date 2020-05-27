185b42db2254677d52752e136a276a6b7574ffdb
Eric LaFlure <ejlaflure@gmail.com>
2020-05-26 19:08:02 -0500

    adding python flask file

---
+++ app.py
@@ -0,0 +1,10 @@
+# Flask Dependency
+from flask import Flask
+
+# Create a New Flask App Instance
+app = Flask(__name__)
+
+# Create Flask Routes with a function
+@app.route('/')
+def hello_world():
+	return 'Hello world'
